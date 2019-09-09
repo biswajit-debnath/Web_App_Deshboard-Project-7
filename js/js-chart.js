046d8523519d22653b0a828d1e5867bd3f7bbde1
shiv <kshivji124@gmail,com>
2019-09-09 17:05:09 +0530

    update

--- js/js-chart.js
+++ js/js-chart.js
@@ -5,12 +5,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
+        labels: ['0','16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
         datasets: [{
             label: 'My First dataset',
             backgroundColor: 'rgba(255, 99, 132,.4)',
             borderColor: 'blue',
-            data: [500,1000,750,1250,1750,1250,1500,1000,1500,2000,1500,2000]
+            data: [0,500,1000,750,1250,1750,1250,1500,1000,1500,2000,1500,2000]
         }]
     },
 
@@ -19,7 +19,50 @@ var chart = new Chart(ctx, {
         elements: {
             line: {
                 tension: 0 // disables bezier curves
+            },
+            point: {
+                radius: 5
             }
+        },
+        
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMax: 2500,
+                    suggestedMin: 0,
+                    stepSize: 500,
+                    
+                    labelOffset: 25,
+                    //padding: 40
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    labelOffset: -45,
+                    //padding: 40
+                    suggestedMax: 66,
+                    suggestedMin: 0,
+                    stepSize: 6,
+                }
+            }]
+
         }
     }
+});
+
+
+
+
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+var myBarChart = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['S','M','T','W','T','F','S'],
+        datasets: [{
+            backgroundColor: 'rgba(255, 99, 132,.4)',
+            data: [0,50,100,150,200,250]
+        }],
+    },
+    options:{} 
 });
